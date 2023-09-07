56dae38d1771815a6018dae07de62b2184742c5d
Christian Nwamba <nwambachristian@gmail.com>
2023-09-07 11:39:44 +0100

    Shorten/Attempt a fetch request

--- pages/api/shorten.js
+++ pages/api/shorten.js
@@ -1,8 +1,15 @@
 import { customAlphabet, urlAlphabet } from "nanoid";
 
 export default async function handler(req, res) {
+  const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT;
+  const GRAPHQL_KEY = process.env.GRAPHQL_KEY;
   const shortCode = customAlphabet(urlAlphabet, 5)();
-  console.log(shortCode)
+
   const response = {};
+  try {
+    const res = await fetch(GRAPHQL_ENDPOINT, options);
+    response.data = await res.json();
+    response.statusCode = 200;
+  } catch (error) {}
   res.status(response.statusCode).json(response.data);
 }
