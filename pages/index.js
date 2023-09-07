3cb8f2252bcd4df7274fa8dafc0452e16e1fc20d
Christian Nwamba <nwambachristian@gmail.com>
2023-09-07 11:22:46 +0100

    Home/Validate URL input

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Link from "next/link";
 
+import isURL from "validator/lib/isURL";
+
 function Home() {
+  const [isURLValid, setIsURLValid] = React.useState(true);
+
   return (
     <main className="grid place-items-center h-screen">
       <div className="bg-cyan-900 w-full grid place-content-center py-20">
@@ -15,7 +19,18 @@ function Home() {
                 name="search"
                 placeholder="Enter link here"
                 id="search"
-                className={`block w-full rounded-md border-0 py-3 px-3 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset outline-none focus:ring-cyan-600`}
+                className={`block w-full rounded-md border-0 py-3 px-3 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset outline-none focus:ring-cyan-600 ${
+                  !isURLValid && "focus:ring-red-600 ring-red-600"
+                } `}
+                onKeyUp={async (e) => {
+                  if (e.key === "Enter") {
+                    if (isURL(e.target.value)) {
+                      setIsURLValid(true);
+                    } else {
+                      setIsURLValid(false);
+                    }
+                  }
+                }}
               />
               <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
                 <kbd className="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-400">
