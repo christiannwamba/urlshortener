c3dd759fe37d5be8659a21ab17c935dc93d1a386
Christian Nwamba <nwambachristian@gmail.com>
2023-09-07 12:32:24 +0100

    Short/Fetch options

--- pages/[short].js
+++ pages/[short].js
@@ -9,6 +9,15 @@ export default Short;
 export async function getServerSideProps(context) {
   const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT;
   const GRAPHQL_KEY = process.env.GRAPHQL_KEY;
+
+  const options = {
+    method: "POST",
+    headers: {
+      "x-api-key": GRAPHQL_KEY,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query, variables }),
+  };
   
   const res = await fetch(GRAPHQL_ENDPOINT, options);
   const data = await res.json();
