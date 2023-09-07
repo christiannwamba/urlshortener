d0a593025d6ca14ff7f26167d412a510d7e6edba
Christian Nwamba <nwambachristian@gmail.com>
2023-09-07 11:35:54 +0100

    Shorten/Generate Short Code

--- pages/api/shorten.js
+++ pages/api/shorten.js
@@ -1,4 +1,8 @@
+import { customAlphabet, urlAlphabet } from "nanoid";
+
 export default async function handler(req, res) {
+  const shortCode = customAlphabet(urlAlphabet, 5)();
+  console.log(shortCode)
   const response = {};
   res.status(response.statusCode).json(response.data);
 }
