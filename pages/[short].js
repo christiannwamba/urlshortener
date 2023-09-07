421e727dd5a4f5de302ee2b6c4b1473d087799fc
Christian Nwamba <nwambachristian@gmail.com>
2023-09-07 12:31:55 +0100

    Short/Attempt a fetch request

--- pages/[short].js
+++ pages/[short].js
@@ -7,9 +7,16 @@ function Short() {
 export default Short;
 
 export async function getServerSideProps(context) {
+  const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT;
+  const GRAPHQL_KEY = process.env.GRAPHQL_KEY;
+  
+  const res = await fetch(GRAPHQL_ENDPOINT, options);
+  const data = await res.json();
+  const url = data.data.listURLS.items[0];
+  console.log(url.long);
   return {
     redirect: {
-      destination: '/',
+      destination: url.long,
     },
   };
 }
